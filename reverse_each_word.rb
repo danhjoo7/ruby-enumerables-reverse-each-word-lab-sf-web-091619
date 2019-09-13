31d1d961e733e85390d9d81a366eadae1ec129be
Daniel Joo <danhjoo@gmail.com>
2019-09-13 23:12:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,10 +5,3 @@ def reverse_each_word(sentence)
   new_array.join(" ")
 end
 
-def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
-  new_array = []
-  words_array.each do |word|
-    new_array.push(word.reverse)
-  new_array
-end 
